1720ae828d2377967e20620a73bd63ab83d477e8
xiaohan <bornwarm@foxmail.com>
2025-06-03 00:25:28 +0800

    Packing into Android Apps.

---
+++ frontend/android/app/src/main/java/com/example/app/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.app;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = (WebView) this.bridge.getWebView();
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            webView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
+    }
+}
