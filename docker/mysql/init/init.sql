6d59a10f6d6b545164765e35e31888a7e5904b50
xiaohan <bornwarm@foxmail.com>
2025-06-02 20:07:05 +0800

    One-click deployment with docker-compose.

---
+++ docker/mysql/init/init.sql
@@ -0,0 +1,10 @@
+-- 设置字符集
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- 如果需要初始化的表和数据，可以在这里添加
+-- 例如：
+-- CREATE TABLE IF NOT EXISTS users ...
+-- INSERT INTO users ...
+
+SET FOREIGN_KEY_CHECKS = 1; 
